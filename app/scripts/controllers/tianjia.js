305a39f3e6a608116b655efa20fba190cc86c8e6
lijianchao <2318911079@qq.com>
2017-03-27 19:43:24 +0800

    3.27 添加正则页面

--- app/scripts/controllers/tianjia.js
+++ app/scripts/controllers/tianjia.js
@@ -1,12 +1,12 @@
 angular.module('wangkelongApp')
 	.controller('tianjiaCtrl', function($scope, $http, $location) {
 		$scope.tij = function() {
-			var spmc = $scope.spmc
-			var djbh = $scope.djbh
-			var dhsl = $scope.dhsl
-			var dhze = $scope.dhze
-			var dhrq = $scope.dhrq
-			var jhj = $scope.jhj
+			var spmc = $scope.userName
+			var djbh = $scope.mobile
+			var dhsl = $scope.cunt
+			var dhze = $scope.ed
+			var dhrq = $scope.rq
+			var jhj = $scope.jh
 				//alert($scope.spmc)
 				/*$scope.spmc = data.data.mingcheng;
 				$scope.djbh = data.data.bianhao;
@@ -38,4 +38,58 @@ angular.module('wangkelongApp')
 		$scope.ss = function(){
 			$location.url("/zhuye")
 		}
+		//验证
+//		function c(){
+//			var shopname = document.getElementById('spmc').value;
+//			var shopnumber = document.getElementById('dhsl').value;
+//			var shopcount = document.getElementById('dhze').value;
+//			var shopdate = document.getElementById('dhrq').value;
+//			var shopprice = document.getElementById('jhj').value;
+//			var aInput = document.getElementsByTagName('input');
+//			var re1=/^[\u4e00-\u9fa5]{2,5}$/;
+//			var re2=/^[0-9]{11}$/;
+//			var re3=/^[\u4e00-\u9fa5]{1,200}$/;
+//			
+//			for(var i=0;i<aInput.length;i++){
+//				aInput[i].onblur = function(){
+//					if(!(re1.test(shopname))){
+//						alert("只能为2或5个汉字。");
+//						return false;
+//					}
+//				}
+//			}
+//		}
+   $scope.save = function () {
+        var myselect=document.getElementById("company");
+        var index=myselect.selectedIndex ; 
+        var companyVal=myselect.options[index].value;
+        var companyText=myselect.options[index].text;
+        //获取到表单是否验证通过
+        if($scope.myForm.$valid){
+            var datajson = {
+                userName:$scope.userName,
+                mobile:$scope.mobile,
+                cunt:$scope.cunt,
+                ed:$scope.ed,
+                rq:$scope.rq,
+                jh:$scope.jh,
+                companyVal:companyVal,
+                companyText:companyText
+            }   
+            console.log(datajson);
+            
+       /* $http({
+            method  : 'POST',
+            url     : '',
+            params    : pData,  
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' } 
+        })
+        .success(function(data) {
+           alert('表单通过');
+        });*/
+
+        }else{
+            alert('表单没有通过验证');
+        }
+    }
 	});
